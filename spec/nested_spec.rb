9f8ebcd10a27b82ef1aa6e8f5b1d14ce0bcb8b17
kenneth gil <astronori@gmail.com>
2019-12-04 03:07:41 +0000

    Done.

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -9,7 +9,7 @@ describe "nested hash" do
     end
   end
 
-  describe "#alan_kay_is_known_for" do
+  describe #alan_kay_is_known_for do
     it "operates on the programmer_hash and returns the value of what Alan Kay is known for" do 
       expect(alan_kay_is_known_for).to eq("Object Orientation")
     end 
