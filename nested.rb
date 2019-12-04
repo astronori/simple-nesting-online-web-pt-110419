feb24e472673951b61a2c8ec88ee52276a1c5f86
kenneth gil <astronori@gmail.com>
2019-12-04 02:44:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -18,9 +18,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
+		programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -35,25 +33,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for] 
 end
 
-def dennis_ritchies_language
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-end
+alan_kay_is_known_for
 
 def adding_matz
 # add the following information to the top level of programmer_hash
